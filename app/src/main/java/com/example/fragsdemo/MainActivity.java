4964ab76756dda88f65ef01ab7313c6eb30e6228
filitov@gmail.com <filitov@gmail.com>
2021-04-12 16:39:03 +0800

    change fragment with getSupportFragmentManager

--- app/src/main/java/com/example/fragsdemo/MainActivity.java
+++ app/src/main/java/com/example/fragsdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.fragsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.fragsdemo.ui.HomeFragment;
+import com.example.fragsdemo.ui.TrendFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.frag_host, HomeFragment.newInstance(), null)
+                        .commit();
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.frag_host, TrendFragment.newInstance(), null)
+                        .commit();
+            }
+        });
+
     }
 }
