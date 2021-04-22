b4597871da0a01034c0dafd5fed37dada8159ec3
filitov@gmail.com <filitov@gmail.com>
2021-04-22 23:20:45 +0800

    use navController

--- app/src/main/java/com/example/fragsdemo/MainActivity.java
+++ app/src/main/java/com/example/fragsdemo/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.fragsdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavController = Navigation.findNavController(this, R.id.frag_host);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNavController.navigate(R.id.trendFragment);
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNavController.navigate(R.id.settingFragment);
+            }
+        });
+
+
     }
 }
